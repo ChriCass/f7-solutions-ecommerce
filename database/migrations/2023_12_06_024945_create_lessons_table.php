a77f4c8218e325d946e18b8cb6245520c411a463
ChriCass <chris_ccc68@yahoo.com.pe>
2023-12-05 21:55:13 -0500

    ADD: Vue frontal integrated logic/styles

--- database/migrations/2023_12_06_024945_create_lessons_table.php
+++ database/migrations/2023_12_06_024945_create_lessons_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('lessons', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('course_id');
+            $table->string('title');
+            $table->string('duration');
+            $table->string('video');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('lessons');
     }
 };
--- database/migrations/2023_12_06_025231_create_reviews_table.php
+++ database/migrations/2023_12_06_025231_create_reviews_table.php
@@ -11,14 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('course_id');
+            $table->integer('rating');
+            $table->text('review');
             $table->timestamps();
         });
     }
@@ -28,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('reviews');
     }
 };
